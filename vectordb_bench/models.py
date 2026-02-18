07ba9e3267673c155979b2d2d0bd4b038bfcd68d
Marius <business@mottosen.xyz>
2026-02-18 15:23:01 +0100

    flatten where results data is placed

--- vectordb_bench/models.py
+++ vectordb_bench/models.py
@@ -252,7 +252,7 @@ class TestResult(BaseModel):
         result_root = config.RESULTS_LOCAL_DIR
         for db, result in db2case.items():
             self.write_db_file(
-                result_dir=result_root.joinpath(db.value),
+                result_dir=result_root,
                 partial=TestResult(
                     run_id=self.run_id,
                     task_label=self.task_label,
